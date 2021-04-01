e21a3714402e06fc911f4158a4e33da821598f04
Marin Pejčin <56229625+peko99@users.noreply.github.com>
2021-04-01 20:04:42 +0200

    Add files via upload

---
+++ setup.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "currency"
+(
+    "currency_id" integer PRIMARY KEY,
+    "currency_name" character varying NOT NULL,
+    "country" character varying NOT NULL,
+    "symbol" character(3) NOT NULL
+);
+
+CREATE TABLE "exchange_rate"
+(
+	"id" serial PRIMARY KEY,
+    "currency_id" integer,
+    "unit" int NOT NULL,
+    "buying_rate" double precision NOT NULL,
+    "selling_rate" double precision NOT NULL,
+	CONSTRAINT fk FOREIGN KEY (currency_id) REFERENCES currency(currency_id)
+);
+
+CREATE TABLE "exchange_transactions"
+(
+	"id" serial PRIMARY KEY,
+	"currency_id" integer NOT NULL,
+	"amount_bought" double precision NOT NULL,
+	"amount_sold" double precision NOT NULL,
+	"transaction_type" character varying NOT NULL
+);
+
+INSERT INTO currency VALUES (0, 'Euro', 'European Union', 'EUR');
+INSERT INTO exchange_rate (currency_id, unit, buying_rate, selling_rate) VALUES (0, 1, 1, 1);
