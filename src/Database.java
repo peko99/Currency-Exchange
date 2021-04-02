9483d793aa875614cf8318913297c16511a48f4c
Marin Pejčin <56229625+peko99@users.noreply.github.com>
2021-04-02 21:38:38 +0200

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -84,4 +84,77 @@ public class Database {
 		
 		executeSql(sql, con);
 	}
+	
+	// Method that prints currencies and their exchange rates
+	public static void printList(Connection con) {
+		// Creating a view table with all the neccesery data
+		String createViewTable = "CREATE OR REPLACE VIEW exchange_table AS\n"
+				+ " SELECT t2.currency_id,\n"
+				+ "    t1.symbol,\n"
+				+ "    t1.currency_name,\n"
+				+ "    t1.country,\n"
+				+ "    t2.unit,\n"
+				+ "    t2.buying_rate,\n"
+				+ "    t2.selling_rate\n"
+				+ "   FROM currency t1,\n"
+				+ "    exchange_rate t2\n"
+				+ "  WHERE t1.currency_id = t2.currency_id;";
+		
+		executeSql(createViewTable, con);
+		
+		try {
+			String viewQuery = "SELECT * FROM exchange_table;";
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery(viewQuery);
+			
+			System.out.println("CODE\t\tNAME\t\tSYMBOL\t\tCOUNTRY\t\tUNIT\t\tBUY RATE\t\tSELL RATE\n");
+			
+			while(rs.next()) {
+				int currencyCode = rs.getInt("currency_id");
+				String currencySymbol = rs.getString("symbol");
+				String currencyName = rs.getString("currency_name");
+				String currencyCountry = rs.getString("country");
+				int currencyUnit = rs.getInt("unit");
+				double buyRate = rs.getDouble("buying_rate");
+				double sellRate = rs.getDouble("selling_rate");
+				
+				System.out.printf("%-4d\t\t%-14s\t%-15s\t%-15s\t%-3d\t\t%-7f\t\t%-7f\n", currencyCode, currencyName, currencySymbol, currencyCountry, currencyUnit, buyRate, sellRate);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	// This method prints data from transactions table
+	public static void printTransactions(Connection con) {
+		String sql = "CREATE OR REPLACE VIEW transactions AS\n"
+				+	"SELECT currency.currency_id, currency.symbol, exchange_transactions.amount_bought, exchange_transactions.amount_sold, exchange_transactions.transaction_type \n"
+				+	"FROM exchange_transactions\n"
+				+	"LEFT JOIN currency ON exchange_transactions.currency_id = currency.currency_id;";
+		
+		executeSql(sql, con);
+		
+		try {
+			String viewQuery = "SELECT * FROM transactions;";
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery(viewQuery);
+			
+			System.out.println("CODE\t\tSYMBOL\t\tAMOUNT BOUGHT\t\tAMOUNT SOLD\t\tTRANSACTION TYPE\n");
+			
+			while(rs.next()) {
+				int currencyCode = rs.getInt("currency_id");
+				String currencySymbol = rs.getString("symbol");
+				double amountBought = rs.getDouble("amount_bought");
+				double amountSold = rs.getDouble("amount_sold");
+				String transactionType = rs.getString("transaction_type");
+				
+				System.out.printf("%-4d\t\t%-3s\t\t%-7f\t\t%-7f\t\t%-6s\n", currencyCode, currencySymbol, amountBought, amountSold, transactionType);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
