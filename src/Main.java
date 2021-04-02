acf297d031d4e971ffac6bb8280ba9821e5a332c
Marin Pejčin <56229625+peko99@users.noreply.github.com>
2021-04-02 21:37:44 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,18 @@ public class Main {
 	public static void displayMenu() {		
 		System.out.println("\nChoose one of the options by pressing a number\n");
 		System.out.println("\t -1) Exit program ");
-		System.out.println("\t 0) Print the Menu ");
-		System.out.println("\t 1) Add a currency ");
-		System.out.println("\t 2) Modify a currency ");
-		System.out.println("\t 3) Delete a currency ");
-		System.out.println("\t 4) Add an exchange (for an existing currency!)");
-		System.out.println("\t 5) Modify an exchange");
-		System.out.println("\t 6) Delete an excgange");
-		System.out.println("\t 7) Buy a currency");
-		System.out.println("\t 8) Sell a currency");
-		System.out.println("\t 9) Clear database");
+		System.out.println("\t  0) Print the Menu ");
+		System.out.println("\t  1) Add a currency ");
+		System.out.println("\t  2) Modify a currency ");
+		System.out.println("\t  3) Delete a currenc9y ");
+		System.out.println("\t  4) Add an exchange (for an existing currency!)");
+		System.out.println("\t  5) Modify an exchange");
+		System.out.println("\t  6) Delete an excgange");
+		System.out.println("\t  7) Buy a currency");
+		System.out.println("\t  8) Sell a currency");
+		System.out.println("\t  9) Clear database");
+		System.out.println("\t 10) Print exchange list");
+		System.out.println("\t 11) Print exchange transactions");
 		System.out.println("");
 	}
 
@@ -26,7 +28,6 @@ public class Main {
 		Connection con = null;		
 		con = Database.getConnection();
 		
-		Scanner choice = new Scanner(System.in);
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Hello! Welcome to Currency Exchange app!");
@@ -36,7 +37,7 @@ public class Main {
 		
 		do {
 			System.out.print("\nYour entry (Press 0 to print Menu) -> ");
-			userChoice = choice.nextInt();
+			userChoice = input.nextInt();
 
 			switch(userChoice) {
 				case -1: 
@@ -74,13 +75,18 @@ public class Main {
 				case 9:
 					Database.resetDatabase(con);
 					break;
+				case 10:
+					Database.printList(con);
+					break;
+				case 11:
+					Database.printTransactions(con);
+					break;
 				default: 
 					System.out.println("Sorry, command is not allowed! Try again\n");
 			}
 		}while(userChoice != -1);
 		
 		input.close();
-		choice.close();
 	}
 }
 		
