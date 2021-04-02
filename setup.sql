234b36df33588884f000a0417ddee9541ae5b48c
Marin Pejčin <56229625+peko99@users.noreply.github.com>
2021-04-02 21:36:22 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,18 +1,18 @@
 CREATE TABLE "currency"
 (
-    "currency_id" integer PRIMARY KEY,
-    "currency_name" character varying NOT NULL,
-    "country" character varying NOT NULL,
-    "symbol" character(3) NOT NULL
+	"currency_id" integer PRIMARY KEY,
+	"currency_name" character varying NOT NULL,
+	"country" character varying NOT NULL,
+	"symbol" character(3) NOT NULL
 );
 
 CREATE TABLE "exchange_rate"
 (
 	"id" serial PRIMARY KEY,
-    "currency_id" integer,
-    "unit" int NOT NULL,
-    "buying_rate" double precision NOT NULL,
-    "selling_rate" double precision NOT NULL,
+	"currency_id" integer,
+	"unit" int NOT NULL,
+	"buying_rate" double precision NOT NULL,
+	"selling_rate" double precision NOT NULL,
 	CONSTRAINT fk FOREIGN KEY (currency_id) REFERENCES currency(currency_id)
 );
 
@@ -22,7 +22,8 @@ CREATE TABLE "exchange_transactions"
 	"currency_id" integer NOT NULL,
 	"amount_bought" double precision NOT NULL,
 	"amount_sold" double precision NOT NULL,
-	"transaction_type" character varying NOT NULL
+	"transaction_type" character varying NOT NULL,
+	CONSTRAINT fk FOREIGN KEY (currency_id) REFERENCES currency(currency_id)
 );
 
 INSERT INTO currency VALUES (0, 'Euro', 'European Union', 'EUR');
